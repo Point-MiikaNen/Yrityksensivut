eb7c4ca7c5563cdc70c3a756328993bbf905acce
Point-MiikaNen <miika.nenonen@student.careeria.fi>
2025-03-03 16:05:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs').promises;
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // Tarjoa staattiset tiedostot public-kansiosta
 app.use(express.static(path.join(__dirname, 'public')));
